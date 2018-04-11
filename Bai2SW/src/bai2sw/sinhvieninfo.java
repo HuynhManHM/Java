4ff5fa48851b9b32163f9f84350102aa22716393
HuynhManHM <quanhuynh1611@gmail.com>
2018-04-11 10:04:45 +0700

    Bài 2 Swing+ArrayList

---
+++ Bai2SW/src/bai2sw/sinhvieninfo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai2sw;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class sinhvieninfo {
+        private String hoten;
+        private String nam;
+        private String gt;
+        public String getten(){ return this.hoten;}
+        public String getnam() {return this.nam;}
+        public String getgt(){return this.gt;}
+        public void setten(String ten1 ){ this.hoten=ten1;}
+        public void setnam(String nam1){this.nam=nam1;}
+        public void setgt(String gt1){this.gt=gt1;}
+}
